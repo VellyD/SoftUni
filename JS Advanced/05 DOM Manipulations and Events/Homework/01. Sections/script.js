a6290a73350341082b13ca3d77708d637c9418e6
unknown <viddvd3@gmail.com>
2021-06-12 12:43:12 +0300

    adding solutions

--- JS Advanced/05 DOM Manipulations and Events/Homework/01. Sections/script.js
+++ JS Advanced/05 DOM Manipulations and Events/Homework/01. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
-}
+   let divToAppend = document.getElementById('content');
+   words.forEach(element => {
+      let divEl = document.createElement('div');
+      let pEl = document.createElement('p');
+      pEl.textContent = element;
+      pEl.style.display = 'none';
+
+      divEl.appendChild(pEl);
+      divToAppend.appendChild(divEl);
+
+      divEl.addEventListener('click', () => {
+         pEl.style.display = pEl.style.display === 'none'? 'block': 'none';
+      });
+
+   });
+};
--- JS Advanced/05 DOM Manipulations and Events/Lab/04. Mouse-Gradient/app.js
+++ JS Advanced/05 DOM Manipulations and Events/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,15 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradientElement = document.getElementById('gradient');
+    let resultelement = document.getElementById('result');
+
+    gradientElement.addEventListener('mousemove', (e) => {
+        let power = e.offsetX / (e.target.clientWidth - 1);
+        power = Math.trunc(power * 100);
+        resultelement.textContent = power + '%';
+    });
+
+    gradientElement.addEventListener('mouseout', () => {
+        resultelement.textContent = "";
+    });
+    
 }
