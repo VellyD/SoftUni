8f69fb58c1608a3b0ee2ad0667b47d29ce64b898
unknown <viddvd3@gmail.com>
2021-06-24 10:27:21 +0300

    add solution

---
+++ JS Advanced/08 Classes/Lab/02. Get Persons.js
@@ -0,0 +1,29 @@
+function solve() {
+    let output = [];
+
+
+    class Person {
+        constructor(firstName, lastName, age, email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+
+        toString() {
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+        }
+    }
+
+    let person = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
+    let person1 = new Person('SoftUni');
+    let person2 = new Person('Stephan', 'Johnson', 25);
+    let person3 = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com');
+
+    output.push(person, person1, person2, person3);
+
+    return output;
+
+}
+
+console.log(solve())
